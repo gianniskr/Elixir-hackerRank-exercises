16a27debcdb44abe2d008fdf7dbdf7c3d6b88996
John Kr <giannis21kr@gmail.com>
2018-10-16 12:24:14 +0300

    Finished exercise

--- Functional Programming/Introduction/ListReplication.exs
+++ Functional Programming/Introduction/ListReplication.exs
@@ -1,32 +1,26 @@
 defmodule Solution do
 
-	# def print(times, e) do
-	# 	IO.puts(e)
-	# 	print(times, times-1, e)
-	# end
+	def print(times, e) do
+		if times > 1 do
+			IO.puts(e)
+			print(times-1, e)
+		else
+			IO.puts( e )
+		end
+	end
 
-	# def print(times,counter, e) do
-	# 	if counter > 0 do
-	# 		IO.puts(e)
-	# 		print(times, counter-1,e)
-	# 	end
-	# end
+	def replicate(list) do
+		[times| listOfNumbers] = list
+		Enum.map(listOfNumbers,fn(x)->print(times,x ) end)
+	end
 
-	# 	def replicate(times, []) do
-	# 		# Nothing
-	# 	end
-
-	# def replicate(times, list) do
-	# 	print(times, hd(list))
-	# 	replicate(times, tl(list))
-	# 	end
-
-		def main() do
-			IO.read(:stdio, :all)
-				|> String.split
-				|> Enum.map(&String.to_integer(&1))
-				|> elements
-    end
+	def main() do
+		IO.read(:stdio, :all)
+			|> String.split
+			|> Enum.map(&String.to_integer(&1))
+			|> replicate
+			end
 end
 
-Solution.replicate(3, [1, 2, 3, 4])
+Solution.main
+
